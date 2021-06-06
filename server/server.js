6c4b093b8ee44f00685f20dbb3ff6befba27eb4b
Eddy Yi <superywan@Eddys-MacBook-Pro.local>
2021-06-07 01:24:20 +0900

    Hopfully this Heroku deploy will success

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "path";
 
 import { notFound } from "./middlewares/errorHandler.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -14,24 +13,21 @@ const app = express();
 dotenv.config();
 
 // Middlewares
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(process.cwd(), "client/build")));
+}
 
 app.use("/api/auth", userRoutes);
 app.use("/api/posts", postRoutes);
 
 app.use(notFound);
 
-// filenames across platforms
-const __dirname = dirname(fileURLToPath(import.meta.url));
-// production.env.NODE_ENV => production or undefined
-if (process.env.NODE_ENV === "production") {
-    // serve build version of react (static files)
-    app.use(express.static(path.join(__dirname, "/client/build")));
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    );
-}
+app.get("*", (req, res) => {
+    res.sendFile(path.join(process.cwd(), "client/build/index.html"));
+});
 
 const listener = app.listen(process.env.PORT || 5000, () => {
     console.log(`Listening on port ${listener.address().port}`);
