a1894218de5b9fbb9d65ab0c0d293da464e7edc2
Eddy Yi <superywan@Eddys-MacBook-Pro.local>
2021-06-07 00:51:53 +0900

    Fixed bugs for heroku deployment 7

--- server/server.js
+++ server/server.js
@@ -17,24 +17,21 @@ dotenv.config();
 app.use(express.json());
 app.use(cors());
 
-// filenames across platforms
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// production.env.NODE_ENV => production or undefined
-if (process.env.NODE_ENV === "production") {
-    // serve build version of react (static files)
-    app.use(express.static(path.join(__dirname, "client/build")));
-}
-
 app.use("/api/auth", userRoutes);
 app.use("/api/posts", postRoutes);
 
 app.use(notFound);
 
-// "catch all" method for the routes that we didn't specified
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
+// filenames across platforms
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// production.env.NODE_ENV => production or undefined
+if (process.env.NODE_ENV === "production") {
+    // serve build version of react (static files)
+    app.use(express.static(path.join(__dirname, "/client/build")));
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
+}
 
 const listener = app.listen(process.env.PORT || 5000, () => {
     console.log(`Listening on port ${listener.address().port}`);
