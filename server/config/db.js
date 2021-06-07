9ed312fef5eed295a655f03ae028f02abc8b34a3
Eddy Yi <superywan@Eddys-MacBook-Pro.local>
2021-06-07 17:16:28 +0900

    Changed Config Pool

--- server/config/db.js
+++ server/config/db.js
@@ -1,23 +1,24 @@
-import pg from "pg";
+import { Pool } from "pg";
+import { parse } from "pg-connection-string";
+
 import dotenv from "dotenv";
 dotenv.config();
 
-// const devConfig = {
-//     user: process.env.PG_USER,
-//     password: process.env.PG_PASSWORD,
-//     host: process.env.PG_HOST,
-//     port: process.env.PG_PORT,
-//     database: process.env.PG_DATABASE,
-// };
+const config = parse(process.env.DATABASE_URL);
+config.ssl = {
+    rejectUnauthorized: false,
+};
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const pool = new Pool(config);
 
-const proConfig = process.env.DATABASE_URL; // heroku PostgreSQL add-on
+// const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const pool = new pg.Pool({
-    connectionString:
-        process.env.NODE_ENV === "production" ? proConfig : devConfig,
-    ssl: { requestCert: true, rejectUnauthorized: false },
-});
+// const proConfig = process.env.DATABASE_URL; // heroku PostgreSQL add-on
+
+// const pool = new pg.Pool({
+//     connectionString:
+//         process.env.NODE_ENV === "production" ? proConfig : devConfig,
+//     ssl: { requestCert: true, rejectUnauthorized: false },
+// });
 
 export default pool;
